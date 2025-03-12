da193442fd461aecaa9915060fed3ecfa5fd50a0
infosetGang <kangyoonkyung@infoset.co.kr>
2025-03-12 14:52:07 +0900

    [feat]  한손모드, rfid 작업 완료

--- resources/ict/js/kiosk/kioskCommon.js
+++ resources/ict/js/kiosk/kioskCommon.js
@@ -2,8 +2,10 @@ $(document).ready(function () {
 	// 한손 모드 클릭 이벤트
 	$('.one_handed_mode').click(function () {
 		const content = $('.content');
+		const scroll = $('.scroll');
 		const image = $(this).find('img');
 		const targetTop = $(window).width() > 1080 ? 1920 : 960;
+		const scrollHeight = $(window).width() > 1080 ? 1560 : 780;
 
 		const isAtTargetTop = parseInt(content.css('top')) === targetTop;
 		content.css({ top: isAtTargetTop ? '0px' : targetTop + 'px' });
@@ -12,11 +14,30 @@ $(document).ready(function () {
 			transform: isAtTargetTop ? 'rotate(0deg)' : 'rotate(180deg)',
 			transition: 'transform 0.3s ease',
 		});
+
+		if (!isAtTargetTop) {
+			scroll.css({
+				'overflow-y': 'scroll',
+				height: scrollHeight + 'px',
+			});
+		} else {
+			scroll.css({
+				'overflow-y': '',
+				height: '100%',
+			});
+		}
 	});
 
 	// 전체 모드 클릭 이벤트
 	$('.full_mode').click(function () {
-		$('.content').css({ top: '0px' });
+		const content = $('.content');
+		const scroll = $('.scroll');
+
+		content.css({ top: '0px' });
+		scroll.css({
+			'overflow-y': '',
+			height: '100%',
+		});
 	});
 
 	// 네비게이션 아이템 활성화
