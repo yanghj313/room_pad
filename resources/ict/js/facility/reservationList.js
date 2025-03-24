bfe7b15ca04bfe880c2422119462227316f12ee8
infosetGang <kangyoonkyung@infoset.co.kr>
2025-03-24 11:01:55 +0900

    [feat] 동반입실자 팝업 작업완료

--- resources/ict/js/facility/reservationList.js
+++ resources/ict/js/facility/reservationList.js
@@ -24,4 +24,38 @@ $(document).ready(function () {
 		$('.pagination_item').removeClass('current_page');
 		$(this).addClass('current_page');
 	});
+
+	// '동반입실' 버튼 클릭 시 팝업 열기
+	$('.overlay').hide();
+
+	$('.status_4').on('click', function () {
+		$('.overlay').fadeIn();
+		$('.join_popup').fadeIn();
+	});
+
+	// '확인' 버튼 클릭 시 팝업 닫기
+	$('.join_popup_close').on('click', function () {
+		$('.overlay').fadeOut();
+		$('.join_popup').fadeOut();
+	});
+
+	// 오버레이 클릭 시 팝업 닫기
+	$('.overlay').on('click', function () {
+		$('.overlay').fadeOut();
+		$('.join_popup').fadeOut();
+	});
+
+	// 동반입실자 추가 버튼 클릭 시
+	$('.add').on('click', function () {
+		let newContent = $('.popup_scroll_content:first').clone();
+		newContent.find('input').val('');
+		$('.popup_scroll').append(newContent);
+	});
+
+	// 동반입실자 삭제 버튼 클릭 시
+	$(document).on('click', '.delete', function () {
+		if ($('.popup_scroll_content').length > 1) {
+			$(this).closest('.popup_scroll_content').remove();
+		}
+	});
 });
