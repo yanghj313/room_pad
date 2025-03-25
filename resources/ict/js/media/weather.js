aadd337a54abc4046450a0886fa1f664c23a0b21
infosetGang <kangyoonkyung@infoset.co.kr>
2025-03-25 15:27:51 +0900

    [feat] 미디어월 소수점 제거

--- resources/ict/js/media/weather.js
+++ resources/ict/js/media/weather.js
@@ -1,15 +1,3 @@
-// 슬릭 오류 방지
-jQuery.event.special.touchstart = {
-	setup: function (_, ns, handle) {
-		this.addEventListener('touchstart', handle, { passive: false });
-	},
-};
-jQuery.event.special.touchmove = {
-	setup: function (_, ns, handle) {
-		this.addEventListener('touchmove', handle, { passive: false });
-	},
-};
-
 // 날씨 > 뉴스 슬라이드 설정
 $(document).ready(function () {
 	$('.news').slick({
